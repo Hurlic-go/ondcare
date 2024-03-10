4d5dd0a301a0e18f633757e96ddf2e96cfaeb579
Hurlic-go <161272685+Hurlic-go@users.noreply.github.com>
2024-03-10 19:28:34 +0900

    Add files via upload

--- send.php
+++ send.php
@@ -15,30 +15,32 @@ if($_POST) {
 
   $subject = 'お問い合わせがありました';
 
-  // \nは、改行の意味。
   $message = "お問い合わせがありました。\n";
   $message .= "\n";
   $message .= "入力された内容は以下の通りです。\n";
   $message .= "---\n";
   $message .= "\n";
   $message .= "お名前：\n";
-  $message .= $_POST["name"]; // name属性がnameの内容が入ります
+  $message .= $_POST["name"];
   $message .= "\n";
   $message .= "メールアドレス:\n";
-  $message .= $_POST["email"]; // name属性がemailの内容が入ります
+  $message .= $_POST["email"]; 
   $message .= "\n";
   $message .= "お問い合わせ本文:\n";
-  $message .= $_POST["message"]; // name属性がmessageの内容が入ります
+  $message .= $_POST["message"];
 
-  //↓最後に、設定した内容でメールを送る命令です
-  if(mb_send_mail($to,$subject,$message)) {
+  if(mb_send_mail($to,$subject,$message))
+   {
     echo "メールが送信されました";
-  } else {
+  }
+   else   {
     echo "メールの送信に失敗しました";
   }
-} else {
+
+ else {
   echo "HTMLからのPOST送信受信に失敗しました";
 }
+?>
    </p>
    </body>
 </html>
