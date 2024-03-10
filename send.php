38875d57eb2c2220886ad4369ab3a14d16dd05d8
Hurlic-go <161272685+Hurlic-go@users.noreply.github.com>
2024-03-10 18:40:33 +0900

    Add files via upload

--- send.php
+++ send.php
@@ -3,23 +3,28 @@
         <meta charset="utf-8" />
     </head>
     <body>
-    <?php
+<p>    <?php
         mb_language("Japanese");
         mb_internal_encoding("UTF-8");
 
-        $to = $_POST['to'];
-        $title = $_POST['title'];
-        $message = $_POST['message'];
-        $headers = "From: ondcare@humanrelightcare.com";
+        $to = 'ondcare@humanrelightcare.com';
+        $belong = $_POST['belong'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $tel = $_POST['tel'];
+        $subject =  $_POST['subject'];
+	    $body = $_POST['body'];
 
-        if(mb_send_mail($to, $title, $message, headers))
+
+        if(mb_send_mail($to, $belong,$name,$email,$tel,$subject,$body))
         {
-            echo "メールは無事に送信されました";
+            echo "お問い合わせ内容は無事に送信されました";
         }
         else
         {
-            echo "メール送信が行えませんでした。再度ご確認ください";
+            echo "お問い合わせ内容の送信が行えませんでした。再度ご確認ください";
         }
     ?>
-    </body>
+   </p>
+   </body>
 </html>
